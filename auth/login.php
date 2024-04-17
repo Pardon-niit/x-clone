792948179cecdc58a2b49b1edb5318d9cd7eff6a
St Pardon <okc4pardon@gmail.com>
2024-04-17 10:04:19 +0100

    :new: added new route and feats

---
+++ auth/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | X</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/x-clone/styles/auth.css">
+</head>
+
+<body>
+    <main class="main">
+        <section class="logo-container">
+            <i class="fa-brands fa-x-twitter auth-logo"></i>
+        </section>
+        <section class="form-container">
+            <form action="" class="auth-form">
+                <h2>Happening now</h2>
+                <h3>Welcome back!</h3>
+                <fieldset>
+                    <input type="text" name="username" placeholder="Email/Username">
+                </fieldset>
+                <fieldset>
+                    <input type="password" name="password" id="password" placeholder="Password">
+                </fieldset>
+                <fieldset class="s-btn">
+                    <input type="submit" value="Login" name="login">
+                </fieldset>
+                <p>Don't have an account? <a href="/x-clone/auth/signup.php">Sign Up</a></p>
+            </form>
+        </section>
+    </main>
+</body>
+
+</html>
---
+++ auth/signup.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup | X</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../styles/auth.css">
+</head>
+
+<body>
+    <main class="main">
+        <section class="logo-container">
+            <i class="fa-brands fa-x-twitter auth-logo"></i>
+        </section>
+        <section class="form-container">
+            <form action="" class="auth-form">
+                <h2>Happening now</h2>
+                <h3>Join today.</h3>
+                <fieldset>
+                    <input type="text" name="username" placeholder="Username">
+                </fieldset>
+                <fieldset>
+                    <input type="text" name="email" placeholder="Email">
+                </fieldset>
+                <fieldset>
+                    <input type="password" name="password" id="password" placeholder="Password">
+                </fieldset>
+                <div>
+                    <input type="checkbox" id="terms">
+                    <label for="terms">By signing up, you agree to the <a href="">Terms of Service</a> and <a href="">Privacy Policy</a>, including <a href="">Cookie</a> Use. </label>
+                </div>
+                <fieldset class="s-btn">
+                    <input type="submit" value="Login" name="login">
+                </fieldset>
+                <p>Already have an account? <a href="/x-clone/auth/login.php">Login</a></p>
+            </form>
+        </section>
+    </main>
+</body>
+
+</html>
